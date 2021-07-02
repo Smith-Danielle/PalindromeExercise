dc53aec90804f707153422b8b679468d9825556d
Smith-Danielle <smith.ddms@gmail.com>
2021-07-01 19:44:25 -0500

    Completed Unit Testing Exercise 2

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+        public bool IsAPalindrome(string input)
+        {
+            bool answer;
+            var inputNew = input.ToList();
+            inputNew.Reverse();
+            var inputString = string.Join("", inputNew);
+            Console.WriteLine(inputString);
+            if (inputString == input)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+            return answer;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("level", true)]
+        [InlineData("noon", true)]
+        [InlineData("danielle", false)]
+        [InlineData("hey", false)]
+        public void Test1(string input1, bool expected)
         {
+            //Arrange
+            var test = new WordSmith();
 
+            //Act
+            var actual = test.IsAPalindrome(input1);
+
+            //Assert
+            Assert.Equal(actual, expected);
         }
     }
 }
